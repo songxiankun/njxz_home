134222e5d432cc53a1ec02a40bcac70833e44faf
Boss <s13584495195@163.com>
2021-01-27 18:56:22 +0800

    web solve kunkun 2021.1.27

--- js/ApiConfig.js
+++ js/ApiConfig.js
@@ -1,5 +1,5 @@
 // var javaBaseUrl = 'http://10.24.216.223:8080/Maintain/';
-let javaBaseUrl = 'http://10.10.130.181:8080/'
-let sxkBaseUrl = 'http://10.10.130.181:52100/'
+let javaBaseUrl = 'http://api.njxzc.edu.cn/'
+let sxkBaseUrl = 'http://api.njxzc.edu.cn/'
 let onlineBaseUrl = 'http://10.10.130.181:8080/'
 let homeUrl = "http://test.njxzc.edu.cn/index.html";
