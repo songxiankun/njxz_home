d0b8dcc87b2a142a63e91ffd1b039294ad42cd02
Kun <s13584495195@163.com>
2021-03-02 08:44:47 +0800

    文件更新以及配置文件更新 kunkun 2021-03-02

--- js/ApiConfig.js
+++ js/ApiConfig.js
@@ -1,3 +1,3 @@
 var javaBaseUrl = 'http://java.home.yanwind.top/'
-var sxkBaseUrl = 'http://api.njxzc.edu.cn/'
-var homeUrl = "http://home.yanwind.top/index.html"
+var sxkBaseUrl = 'http://10.11.124.4:32767/'
+var homeUrl = "http://10.11.124.4/index.html"
