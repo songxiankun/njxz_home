d75b0db338a3fce0c8b7117490679df642567d20
YanWind <2460984485@qq.com>
2021-03-21 19:12:13 +0800

    javaBaseUrl改为线上地址

--- js/ApiConfig.js
+++ js/ApiConfig.js
@@ -1,3 +1,3 @@
-var javaBaseUrl = 'http://java.home.yanwind.top/'
+var javaBaseUrl = 'http://10.11.124.4:8082/Maintain/'
 var sxkBaseUrl = 'http://10.11.124.4:32767/'
 var homeUrl = "http://10.11.124.4/index.html"
