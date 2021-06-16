cc3b26fddcdb9fe505b4d4f448ab9a90b642325e
马连峰 <malianfeng@wanmi.com>
2021-06-16 19:13:30 +0800

    修改接口基址

--- js/ApiConfig.js
+++ js/ApiConfig.js
@@ -1,4 +1,3 @@
-// var javaBaseUrl = 'http://10.11.124.4:8082/Maintain/'
-var javaBaseUrl = 'http://127.0.0.1:8080/Maintain/'
+var javaBaseUrl = 'http://10.11.124.4:8082/Maintain/'
 var sxkBaseUrl = 'http://10.11.124.4:32767/'
 var homeUrl = "http://10.11.124.4/index.html"
